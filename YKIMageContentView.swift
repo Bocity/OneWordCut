3449f61eba49b1fa1cf4e736bb8030b866b437b8
Bocity <haolovej@vip.qq.com>
2019-01-10 12:37:38 +0800

    first commit

---
+++ YKIMageContentView.swift
@@ -0,0 +1,21 @@
+//
+//  YKIMageContentView.swift
+//  OneWordCut
+//
+//  Created by 郝进 on 2019/1/9.
+//  Copyright © 2019年 Bocity. All rights reserved.
+//
+
+import UIKit
+
+class YKIMageContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
